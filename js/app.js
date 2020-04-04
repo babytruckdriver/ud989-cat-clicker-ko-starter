4148376f5aa09ec821a7c4adaf07d8462fbe014e
Gonzalez Izquierdo <essamu@softwareag.com>
2020-04-04 12:56:41 +0200

    Se encapsula el model "Cat" en su propia clase.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,13 @@
 console.log("Cat Clicker, Knockoutjs flavor!");
 
-var viewModel = function () {
+var Cat = function () {
     this.clickCount = ko.observable(0);
     this.name = ko.observable("Tabby");
     this.imgSrc = ko.observable("img/22252709_010df3379e_z.jpg");
     this.imgAttibution = ko.observable("https://www.flickr.com");
 
-    this.nickNames = ko.observableArray(); //["Tidy", "Kity", "Purchi"]);
-    this.nickNames.push("Tidy");
-    this.nickNames.push("Pony");
-    this.nickNames.push("Purchi");
+    this.nickNames = ko.observableArray(["Tidy", "Kity", "Purchi"]); //["Tidy", "Kity", "Purchi"]);
+    //this.nickNames.push("Ponny");
 
     this.level = ko.computed(function () {
         if (this.clickCount() <= 10) {
@@ -20,10 +18,14 @@ var viewModel = function () {
             return "Teen";
         }
     }, this);
+}
 
+var viewModel = function () {
+
+    this.currentCat = ko.observable(new Cat());
 
     this.incrementCounter = function () {
-        this.clickCount(this.clickCount() + 1);
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
     };
 }
 
